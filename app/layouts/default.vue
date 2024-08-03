0b7baf0627fdbea27b97b029f6b23cdc6203d4f5
krane <christian.kranabetter@protonmail.com>
2024-08-03 10:17:36 +0200

    setup project

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+	<main>
+		<slot />
+	</main>
+</template>
+<script setup lang="ts">
+import NavHeader from "~/components/NavHeader.vue";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
+	modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
 	future: {
 		compatibilityVersion: 4,
 	},
